371326c29729f76ee6848d3d76caf69496e3ee61
HANXU2018 <1076998404@qq.com>
2021-07-24 19:10:51 +0800

    Update config.js

--- apijson-doc/.vuepress/config.js
+++ apijson-doc/.vuepress/config.js
@@ -112,6 +112,20 @@ module.exports = {
                         }
                     }
                 },
+                {
+                  icon: '',
+                  name: 'Github搜索',
+                  popover: {
+                    type: 'text',
+                    title: '纯文本说明',
+                    text: '这是一个纯文本的内容展示，就是一段话',
+                    more: {
+                        newWindow: true,
+                        link: 'https://github.com/HANXU2018/APIJSON-DOC/search?q=%E4%B8%B4%E6%97%B6%E6%90%9C%E7%B4%A2',
+                        name: '在Github仓库内搜索'
+                    }
+                  }
+                },
             ]
         }
         ],
